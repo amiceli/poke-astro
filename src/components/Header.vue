b1b6d31bb2da29abb6a0dda6a4224caf4a6f60c9
amiceli <amiceli@letsignit.com>
2022-09-23 23:47:15 +0200

    feat(component): add global components
    - Add header component
    - Add loder component (lit element)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,52 @@
 <template>
     <nav class="navbar">
-        <a
-            href="#"
-            class="navbar-brand"
-        >
-            <img
+        <a href="#" class="navbar-brand">
+            <img 
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png"
                 alt="pikachu"
-            >
+            />
             {{ name }}
         </a>
         <span class="navbar-text text-monospace">v1.0</span>
+        <ul class="navbar-nav d-none d-md-flex">
+            <li class="nav-item active">
+                <a href="/quizz" class="nav-link">
+                    PokéQuizz
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/" class="nav-link">
+                    Pokédex light
+                </a>
+            </li>
+        </ul>
+        <div class=" d-md-flex ml-auto">
+            <FlagsChooser />
+        </div>
     </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+    import { defineComponent } from "vue"
+    import FlagsChooser from '../useCases/changeLang/FlagsChooser.vue'
 
-export default defineComponent({
-    setup() {
-        return { name: 'Pokastro' }
-    }
-})
+    export default defineComponent({
+        components: { FlagsChooser },
+        setup() {
+            return { name: 'Pokastro' }
+        }
+    })
 </script>
 
-<style lang="css" scoped>
-.navbar img {
-    width: 50px;
-    height: 50px;
-}
+<style lang="scss" scoped>
+    .navbar img {
+        width: 50px;
+        height: 50px;
+    }
+
+    .navbar {
+        .d-md-flex {
+            margin-right: 20px;
+        }
+    }
 </style>
