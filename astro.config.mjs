3b844a613177ef9ce8af7e1b771f69e4849591ef
amiceli <amiceli@letsignit.com>
2022-09-23 23:45:25 +0200

    chore(astro): update astro config and package to use lit component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), svelte()]
+  integrations: [vue(), svelte(), lit()],
 });
