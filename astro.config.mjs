08d871789eb434bb1200d5d9265a661173dc8950
amiceli <amiceli@letsignit.com>
2022-09-27 22:17:29 +0200

    chore(npm): add solidjs deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
-
 import lit from "@astrojs/lit";
+
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), svelte(), lit()],
+  integrations: [vue(), svelte(), lit(), solid()]
 });
