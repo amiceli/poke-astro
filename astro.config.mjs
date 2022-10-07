05b28100078d9c4a5fab77c18d540c86721a1b13
amiceli <amiceli@letsignit.com>
2022-10-07 12:41:02 +0200

    chore(ypo): format some code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
 import lit from "@astrojs/lit";
-
 import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
